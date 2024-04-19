2b88a400be9be3732a31bc4ae0e26adb824a0e2c
Mary <69020003+mary-bauman@users.noreply.github.com>
2024-04-19 19:34:13 -0400

    start 200.ts

---
+++ 200/200.ts
@@ -0,0 +1,10 @@
+function numIslands(grid: string[][]): number {
+    function dfs(i: number, j: number): void{
+        grid[i][j] = '0';
+    }
+
+    var num_islands: number = 0;
+
+
+    return num_islands;
+};
