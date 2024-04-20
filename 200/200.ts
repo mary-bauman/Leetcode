65d9942cf7d0853aa865482b4c516f9ac1a5422f
Mary <69020003+mary-bauman@users.noreply.github.com>
2024-04-19 20:26:05 -0400

    wrong answer

--- 200/200.ts
+++ 200/200.ts
@@ -1,10 +1,19 @@
 function numIslands(grid: string[][]): number {
+    if (grid.length==0){
+        return 0;}
     function dfs(i: number, j: number): void{
+        if (i < 0 || j<0 || i >= grid.length || j >= grid.length || (grid[i][j]!=='1')){
+            return;}
         grid[i][j] = '0';
-    }
-
+        dfs(i-1,j);
+        dfs(i+1,j);
+        dfs(i,j-1);
+        dfs(i,j+1);}
     var num_islands: number = 0;
-
-
+    for (var i: number = 0; i < grid.length; i++){
+        for (var j: number = 0; j < grid[0].length; j++){
+            if (grid[i][j]=='1'){
+                num_islands+=1;
+                dfs(i,j);}}}
     return num_islands;
 };
