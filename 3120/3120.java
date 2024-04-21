03b7c3d1f8f41235858e7a803c92f4b02fa3625d
Mary <69020003+mary-bauman@users.noreply.github.com>
2024-04-21 12:02:37 -0400

    Update and rename 3120_from_python.java to 3120.java

--- 3120/3120.java
+++ 3120/3120.java
@@ -1,12 +1,8 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
         Set<Character> lowercase = new HashSet<>();
+
         for (int i = 0; i < word.length(); i++) {
             if (Character.isLowerCase(word.charAt(i)) && word.contains(Character.toUpperCase(word.charAt(i)) + "")) {
-                lowercase.add(word.charAt(i));
-            }
-        }
-
-        return lowercase.size();
-    }
-}
+                lowercase.add(word.charAt(i));}}
+        return lowercase.size();}}
