aae473065af4c9c7a241e165c8ee924d5538e3a0
Mary <69020003+mary-bauman@users.noreply.github.com>
2024-05-23 19:44:41 -0400

    accepted
    Runtime
    2402 ms Beats 33.33%of users with TypeScript
    Memory
    58.04 MB Beats 33.33% of users with TypeScript

--- 2597/2597.ts
+++ 2597/2597.ts
@@ -1,16 +1,9 @@
 function beautifulSubsets(nums: number[], k: number): number {
     function dfs(nums: number[], k: number, i: number, s: Set<number>){
-        if (i == nums.length){
-            if (s.size === 0){return 0;}
-            return 1;
-        }
+        if (i == nums.length){return s.size === 0 ? 0 : 1;}
         let count = dfs(nums, k, i+1, s);
         if (s.has(nums[i]-k)){return count;}
-        let newS: Set<number>  = new Set(s);
-        newS.add(nums[i])
-        return count + dfs(nums,k,i+1,newS);
+        return count + dfs(nums,k,i+1,new Set([...s, nums[i]]));
     }
-
-    nums.sort((a, b) => a - b);
-    return dfs(nums,k, 0, new Set<number>());
+    return dfs(nums.sort((a, b) => a - b), k, 0, new Set<number>());
 };
