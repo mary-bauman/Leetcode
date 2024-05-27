3c84e9de58c3da4852f6aa92ae8eb332967c21a4
Mary <69020003+mary-bauman@users.noreply.github.com>
2024-05-27 09:31:39 -0400

    accepted one-liner

--- 1608/1608.py
+++ 1608/1608.py
@@ -1,7 +1,5 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        for i in range(0,max(nums)+1):
-            if i==sum(1 for n in nums if n >= i):
-                return i
-        return -1
+        return next((i for i in range(0, max(nums) + 1) if i == sum(1 for n in nums if n >= i)), -1)
+
         
