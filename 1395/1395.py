752758d050c154279e354a5aab05dae204369099
Mary <69020003+mary-bauman@users.noreply.github.com>
2024-07-28 21:37:32 -0400

    Accepted dp

--- 1395/1395.py
+++ 1395/1395.py
@@ -1,11 +1,24 @@
+#inspired by https://leetcode.com/u/toho42/
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
-        total = 0
-        n = len(rating)
-        for i in range(n):
-            for j in range(i+1,n):
-                for k in range(j+1,n):
-                     if (rating[i]<rating[j]<rating[k]) or (rating[i]>rating[j]>rating[k]):
-                        total+=1
 
-        return total
+        dp, count = [[0,0] for _ in range(len(rating))], 0
+
+        for i in range(1, len(rating)):
+            greaterThans, lessThans = 0,0
+
+            for j in range(i):
+                if rating[i] > rating[j]:
+                    greaterThans += 1
+                elif rating[i] < rating[j]:
+                    lessThans += 1
+
+            dp[i] = [greaterThans, lessThans]
+
+            for j in range(i):
+                if rating[i] > rating[j]:
+                    count += dp[j][0]
+                elif rating[i] < rating[j]:
+                    count += dp[j][1]
+        print(dp)
+        return count
