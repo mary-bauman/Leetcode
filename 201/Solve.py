c926b947fd3c32dd80b401f8a558eeafd10dda11
Mary <marybauman503@gmail.com>
2025-05-14 22:48:54 -0400

    icon

---
+++ 201/Solve.py
@@ -0,0 +1,8 @@
+def rangeBitwiseAnd(left, right):
+    shift = 0
+    while left < right:
+        left >>= 1
+        right >>= 1
+        shift += 1
+    return left << shift 
+print(rangeBitwiseAnd(5, 700))
