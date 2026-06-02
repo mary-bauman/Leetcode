57dba596013f2e351ecb424fcb50feb444765378
Mary <69020003+mary-bauman@users.noreply.github.com>
2026-06-01 20:19:29 -0400

    accepted

---
+++ 3633/3633.py
@@ -0,0 +1,28 @@
+class Solution:
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        #land first
+        #get earliest land end time
+        earliestLandEnd = inf
+        for i in range(len(landStartTime)):
+            earliestLandEnd = min(earliestLandEnd, landStartTime[i]+landDuration[i])
+        #now earliest water that starts after earliestLandEnd
+        option1 = inf
+        for i in range(len(waterStartTime)):
+            if waterStartTime[i]>=earliestLandEnd:
+                option1 = min(option1, waterStartTime[i]+waterDuration[i])
+            else:
+                option1 = min(option1, waterStartTime[i]+waterDuration[i]+(earliestLandEnd-waterStartTime[i]))
+
+        #water first
+        earliestWaterEnd = inf
+        for i in range(len(waterStartTime)):
+            earliestWaterEnd = min(earliestWaterEnd, waterStartTime[i]+waterDuration[i])
+        option2 = inf
+        for i in range(len(landStartTime)):
+            if landStartTime[i]>=earliestWaterEnd:
+                option2 = min(option2, landStartTime[i]+landDuration[i])
+            else:
+                option2 = min(option2, landStartTime[i]+landDuration[i]+(earliestWaterEnd-landStartTime[i]))
+
+        return min(option1, option2)
+        
